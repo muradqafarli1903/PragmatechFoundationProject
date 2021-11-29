da874b3c435fbc4edc0b6cb0fdd046a6d09a92fe
Murad Qafarli <muradq469@gmail.com.com>
2021-11-29 05:06:52 +0400

    cox xeta var, umid edirem duzelde bilerem

---
+++ flask/register.py
@@ -0,0 +1,91 @@
+users=[]
+simvollar=['\?','\+','\*','\!','\%']
+import re
+import time
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+class register:
+    def __init__(self,_ad,_soyad,_password,_mail,_nomre):
+        self.ad=_ad
+        self.soyad=_soyad
+        self.password=_password
+        self.mail=_mail
+        self.nomre=_nomre
+        users.append(self)
+    def qeydiyyat():
+        global ad,soyad,mail,password,nomre,operator
+        print('zehmet olmasa asagidaki xanalari doldurun...')
+        ad=input('Adiniz:(doldurulmasi zeruridir) ')
+        soyad=input('Soyadiniz:(doldurulmasi zeruridir) ')
+        mail=input('mail: ')
+        operator=input('Prefiks: Nar: 70 ve ya 77, Azercell: 50,Bakcell: 55 ve ya 99 ')
+        nomre=input('Nomre: +994 {}'.format(operator))
+        password=input("sifre: (doldurulmasi zeruridir) ")
+    
+def adYoxlama():
+    global ad
+    while True:
+        if ad==" ":
+            print('adinizi daxil edin')
+            ad=input('Adiniz:(doldurulmasi zeruridir) ')
+        else:
+            break
+def soyadYoxlama():
+    global soyad
+    while True:
+        if soyad==" ":
+            print('soyadinizi daxil edin')
+            soyad=input('Soyadiniz:(doldurulmasi zeruridir) ')
+        else:
+            break
+def nomreYoxlama():
+    global nomre
+    global operator
+    
+    while True:
+        if len(nomre)!=7:
+            nomre=input('duzgun nomre daxil edin: +994 {}'.format(operator))
+            
+            
+        else:
+            break
+        try:
+            nomre=int(nomre)
+        except:
+            nomre=input('nomre simvol qebul ede bilmez, yeniden cehd edin: +994 {}'.format(operator))
+def mailYoxlama():
+    global mail
+    
+    while True:
+        if (re.fullmatch(regex,mail)):
+            print('mail duzgundur. ')
+            break
+        else:
+            mail=input('Mail yanlisdir, yeniden cehd edin')
+def sifreYoxlama():
+    global password
+    if len(password)<8:
+        raise Exception('Sifre en az 8 simvol olmalidir')
+    elif not re.search('[a-z]',password):
+        raise Exception('sifrede en az bir kicik herf olmalidir')
+    elif not re.search('[A-Z]',password):
+        raise Exception('sifrede en az 1 boyuk herf olmalidir')
+    elif not re.search('[0-9]',password):
+        raise Exception('sifrede en az reqem olmalidir')
+    elif not re.search(str(simvollar),password):
+        raise Exception('Sifrede en az bir simvol olmalidir: * % + ? !')
+    else:
+        print('Sifre yoxlanilir...')
+        time.sleep(1)
+        print('Sifre duzgundur')
+    while True:
+        try:
+            pass
+        except Exception as xeta:
+            print(xeta)
+register.qeydiyyat()
+adYoxlama()
+soyadYoxlama()
+mailYoxlama()
+nomreYoxlama()
+
+sifreYoxlama()
